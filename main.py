57dc426199a3e2281dfe04b363c27da60be16bce
Pedro Grangeiro <46334439+pedrograngeiro@users.noreply.github.com>
2024-11-04 13:15:53 -0300

    Example of transcription, text and srt format

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import os
-
 import whisper
 from whisper.utils import get_writer
 
-model = whisper.load_model('small')
+
+def transcribe_audio(audio_file, model_name='small', output_directory='./srts/'):
+    model = whisper.load_model(model_name)
+    result = model.transcribe(audio_file, fp16=False)
+
+    srt_writer = get_writer("srt", output_directory)
+    srt_writer(result, audio_file)
+
+    audio_filename = os.path.basename(audio_file)
+    txt_filename = os.path.splitext(audio_filename)[0] + ".txt"
+
+    with open(os.path.join(output_directory, txt_filename), "w", encoding="utf-8") as txt:
+        txt.write(result["text"])
+
+
+# Usage
 audio = "audios/IELTS-Speaking-Test-Perfect-Band-9-IELTS-Advantage.mp3"
-result = model.transcribe(audio, fp16=False)
-output_directory = "./srts/"
-
-srt_writer = get_writer("srt", output_directory)
-srt_writer(result, audio)
-
-audio_filename = os.path.basename(audio)
-txt_filename = os.path.splitext(audio_filename)[0] + ".txt"
-
-with open(os.path.join(output_directory, txt_filename), "w", encoding="utf-8") as txt:
-    txt.write(result["text"])
-
+transcribe_audio(audio)
